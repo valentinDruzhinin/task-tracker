e37dd4ce223a88e3170ca5ce816ab62a302294ff
vdruzhinin <vdruzhinin@spscommerce.com>
2019-10-19 19:36:55 +0300

    Add PORT env var

--- run.py
+++ run.py
@@ -7,4 +7,6 @@ app = app.create_app()
 
 
 if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', debug=DEBUG)
